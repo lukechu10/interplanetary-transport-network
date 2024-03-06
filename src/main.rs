9020a3315f27bdefdf7987630849e3530bfa091f
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-03-06 18:35:47 +0000

    Remove some duplicate code

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ pub mod halo_orbits;
 pub mod halo_orbits_compute;
 pub mod leo_to_moon;
 pub mod leo_to_moon_compute;
-pub mod leo_to_moon_no_sun;
-pub mod leo_to_moon_test;
 pub mod manifolds_earth_moon;
 pub mod manifolds_sun_earth;
 pub mod multi_planet;
@@ -27,9 +25,7 @@ fn main() {
         "single_planet" => single_planet::start(),
         "multi_planet" => multi_planet::start(),
         "leo_to_moon" => leo_to_moon::start(),
-        "leo_to_moon_test" => leo_to_moon_test::start(),
         "leo_to_moon_compute" => leo_to_moon_compute::start(),
-        "leo_to_moon_no_sun" => leo_to_moon_no_sun::start(),
         "halo_orbits_earth_moon" => halo_orbits::start_earth_moon(),
         "halo_orbits_sun_earth" => halo_orbits::start_sun_earth(),
         "halo_orbits_compute_earth_moon" => halo_orbits_compute::start_earth_moon(),
